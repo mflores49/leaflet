86033b76c1933c205fc6d918b6be6c7eea25da9b
Mflores <estandarsig@gmail.com>
2024-11-19 10:08:06 -0300

    ac1116

--- data/js/maps.js
+++ data/js/maps.js
@@ -1,53 +1,84 @@
-var map = L.map("map").setView([-41.529218, -72.810565], 8);
+// var map = L.map("map").setView([-41.529218, -72.810565], 8);
+
+var map = L.map("map", {
+    center: [-41.529218, -72.810565], // Coordenadas del centro
+    zoom:10, // Nivel de zoom inicial
+    minZoom:7.5, // Nivel mínimo de zoom
+    maxZoom: 16 // Nivel máximo de zoom
+  });
+  
+
+// var googleStreets = L.tileLayer("https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}" ,{
+//     attribution: '  Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE | &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
+//      '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
+//      '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
+//     ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
+//     ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
+
+// });
 
 
+// var traffic = L.tileLayer("https://mt1.google.com/vt?lyrs=h@159000000,traffic|seconds_into_week:-1&style=3&x={x}&y={y}&z={z}",{
+//     attribution: ' Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
+//      '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
+//      '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
+//     ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
+//     ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
 
-var googleStreets = L.tileLayer("https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}" ,{
-    attribution: '  Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE | &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
-     '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
-     '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
-    ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
-    ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
+// } );
+// var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png" ,{
+//     attribution: ' Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
+//      '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
+//      '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
+//     ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
+//     ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
 
-});
+// });
 
+// var googleSat1 = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',{
+//     attribution: 'Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
+//      '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
+//      '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
+//     ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
+//     ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
 
-var traffic = L.tileLayer("https://mt1.google.com/vt?lyrs=h@159000000,traffic|seconds_into_week:-1&style=3&x={x}&y={y}&z={z}",{
-    attribution: ' Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
-     '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
-     '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
-    ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
-    ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
-
-} );
-var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png" ,{
-    attribution: ' Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
-     '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
-     '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
-    ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
-    ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
-
-});
-
-var googleSat1 = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',{
-    attribution: 'Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
-     '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
-     '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
-    ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
-    ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
-
-}).addTo(map);
+// }).addTo(map);
 
 
-var d = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}' ,{
-    attribution: ' Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
-     '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
-     '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
-    ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
-    ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
+// var d = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}' ,{
+//     attribution: ' Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; <a href="https://censo2024.ine.gob.cl/" target="_blank">Censo 2024 INE</a>| ' +
+//      '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
+//      '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> |'+
+//     ' <a href=" https://firms.modaps.eosdis.nasa.gov/map/#d:today,today;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.87,-41.35,10.40z" target="_blank"> Firms-NASA hoy </a> | '+
+//     ' Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank"> Linkedin </a> '       
 
-});
+// });
 var googleSat = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}');
+
+// Función para crear capas con una atribución común
+function createTileLayer(url, attribution) {
+    return L.tileLayer(url, {
+        attribution: attribution
+    });
+}
+
+// Atribución común para todas las capas
+var commonAttribution = 'Plataforma de Análisis y Gestión Territorial - Datum WGS-84 Proyección Geográfica CHILE| &copy; ' + 
+    '<a href="https://censo2024.ine.gob.cl/" target="_blank">Censo INE</a>| ' +
+    '<a href="https://mapas.mop.gov.cl/" target="_blank">Mapas MOP</a> | ' +
+    '<a href="https://www.windy.com/es/-Temperatura-temp?temp,-41.456,-72.933,11" target="_blank">Windy</a> | ' +
+    '<a href="https://firms.modaps.eosdis.nasa.gov/map/#d:24hrs,24hrs;l:fires_all,countries,landsat_human,protected_areas,protected_areas_regional,volcanoes,earth;@-72.89,-41.46,10.64z" target="_blank">Firms-NASA 24hrs</a> | ' +
+    'Desarrollo <a href="https://www.linkedin.com/in/marcela-flores-ponce/" target="_blank">Linkedin</a>';
+
+// Definición de las capas
+var googleStreets = createTileLayer("https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}", commonAttribution);
+var traffic = createTileLayer("https://mt1.google.com/vt?lyrs=h@159000000,traffic|seconds_into_week:-1&style=3&x={x}&y={y}&z={z}", commonAttribution);
+var osm = createTileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", commonAttribution);
+var googleSat1 = createTileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', commonAttribution);
+var d = createTileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', commonAttribution);
+
+// Añadir la capa predeterminada al mapa
+googleSat1.addTo(map);
 
 
 
@@ -100,7 +131,7 @@ cluster_Er.addLayer(edificacion_rural3p);
 
 var hotspotsLayer = L.esri.featureLayer({
                             url: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Satellite_VIIRS_Thermal_Hotspots_and_Fire_Activity/FeatureServer/0'
-                            });
+                            }).addTo(map);
 
 //Agregar el control de coordenadas
 L.control.coordinates(
